0694f7675c802923dfe96f080a5685dd6d2ba8a5
rtektas06 <ruzgartektass@gmail.com>
2026-03-02 01:32:43 +0300

    nem eklendi

--- app.py
+++ app.py
@@ -2,12 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Örnek sıcaklık değeri
+# Örnek değerler (sabit)
 current_temp = 24.5
+current_humidity = 55.2
 
 @app.route("/")
 def home():
-    return render_template("index.html", temp=current_temp)
+    return render_template("index.html", temp=current_temp, humidity=current_humidity)
 
 if __name__ == "__main__":
     app.run()
